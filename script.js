89e35f29a04b26c8ba4349aa07dbdd5c9a4388e7
Micolexy <popoolamicheal39@gmail.com>
2023-12-13 05:47:33 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const menu = document.querySelector('#menu');
+
+function openmenu() {
+    menu.classList.add('menushown');
+}
+function closemenu() {
+    menu.classList.remove('menushown');
+}
